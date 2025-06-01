6f2649ffc69187c1be3c5b5aa6ae8cbb2a53d19f
DiefersonMatos <diefersonamatos@gmail.com>
2025-06-01 22:17:37 +0100

    Feat: Add javadoc and package-info

--- application-module/src/main/java/com/matos/application/Application.java
+++ application-module/src/main/java/com/matos/application/Application.java
@@ -5,10 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Main Spring Boot application class for MetalFest.
+ * <p>
+ * This class bootstraps the entire application, enabling component scanning across
+ * the "com.matos" package, JPA repository support, and entity scanning.
+ * <p>
+ * To start the application, run the {@code main} method.
+ */
 @SpringBootApplication(scanBasePackages = "com.matos")
 @EnableJpaRepositories(basePackages = "com.matos.persistence.repository")
 @EntityScan(basePackages = "com.matos.persistence.entity")
 public class Application {
+
+    /**
+     * Application entry point.
+     *
+     * @param args command-line arguments passed at startup
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ business-module/src/main/java/com/matos/business/user/boundary/package-info.java
@@ -0,0 +1,1 @@
+package com.matos.business.user.boundary;
